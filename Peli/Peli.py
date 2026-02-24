618bc1eb43b4959bcc359b1e4247aeee6be190c0
lauripussinen <lauri.pussinen@gmail.com>
2026-02-24 15:42:59 +0200

    Peli

--- Peli/Peli.py
+++ Peli/Peli.py
@@ -1,9 +1,1 @@
-import random
-import tarina
-
-
-alkutarina = input('Haluatko lukea pelin alkutarinan? (Kyllä/Ei): ')
-if alkutarina == 'Kyllä':
-    # print wrapped string line by line
-    for line in tarina.haetarina():
-        print(line)
+#kesken
