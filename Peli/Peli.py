c6eba38fa3217d8f2bd51600f54c680ba4463ded
lauripussinen <lauri.pussinen@gmail.com>
2026-02-24 12:58:22 +0200

    Peli

--- Peli/Peli.py
+++ Peli/Peli.py
@@ -1,14 +1,9 @@
 import random
-import story
-from geopy import distance
+import tarina
 
-import mysql.connector
 
-conn = mysql.connector.connect(
-    host='localhost',
-    port=3306,
-    database='some_database_name', #ei vielä tiedossa
-    user='some_user_name', #ei cielä tiedossa
-    password='some_password', #ei vielä tiedossa
-    autocommit=True
-)
+alkutarina = input('Haluatko lukea pelin alkutarinan? (Kyllä/Ei): ')
+if alkutarina == 'Kyllä':
+    # print wrapped string line by line
+    for line in tarina.haetarina():
+        print(line)
