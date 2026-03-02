8c4fa009b25df38b6f12598caae64fb740c51d5c
lauripussinen <lauri.pussinen@gmail.com>
2026-03-02 13:42:51 +0200

    Laurin jutut

--- Laurin jutut/tietokannan luonti skripti.py
+++ Laurin jutut/tietokannan luonti skripti.py
@@ -1,92 +1,40 @@
-create
-database
-ohjelmistopeli;
+# 1. luo aluksi uusi tietokanta.
+create database ohjelmistopeli;
 
-source(mulla
-on
-tässä
-seuraavanlainen
-koodi: C:\o1\lp1.sql;)
+#2. käytä uutta luotua tietokantaa.
+use ohjelmistopeli;
 
-SET
-FOREIGN_KEY_CHECKS = 0;
+#3. lisää tietokantaan sql tiedosto, jota on käytetty flight_game:ssa.
+source C:\o1\lp1.sql;
 
-DROP
-TABLE
-IF
-EXISTS
-game;
-DROP
-TABLE
-IF
-EXISTS
-goal;
-DROP
-TABLE
-IF
-EXISTS
-goal_reached;
-DROP
-TABLE
-IF
-EXISTS
-ports;
-SET
-FOREIGN_KEY_CHECKS = 1;
+#4. poistetaan tuodusta qsl- tiedostosta game, goal ja goal_reached-taulut.
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS game;
+DROP TABLE IF EXISTS goal;
+DROP TABLE IF EXISTS goal_reached;
+SET FOREIGN_KEY_CHECKS = 1;
 
-CREATE
-TABLE
-game(
-    id
-INT
-AUTO_INCREMENT
-PRIMARY
-KEY,
-screen_name
-VARCHAR(40)
-NULL,
-location
-VARCHAR(40)
-NULL,
-co2_consumed
-FLOAT
-DEFAULT
-0,
-co2_budget
-FLOAT
-DEFAULT
-5000,
-current_item
-INT
-DEFAULT
-0,
-attempts
-INT
-DEFAULT
-0
+#5. luodaan ensiksi uusi game-taulu.
+CREATE TABLE game (
+	id int auto_increment primary key,
+    screen_name   varchar(40) NULL,
+    location      varchar(40) NULL,
+    co2_consumed  float default 0,
+    co2_budget    float default 5000,
+    current_item  int default 0,
+    attempts      int default 0
 )
-DEFAULT
-CHARSET = latin1;
+	charset=latin1;
 
-create
-table
-item
-(
-    id           int auto_increment
-primary key,
-nimi         varchar(100)
-null,
-maa
-varchar(40)
-null,
-vihje1
-varchar(255)
-null,
-vihje2
-varchar(255)
-null,
-vihje3
-varchar(255)
-null
-)
-charset = latin1;
+#6. luodaan lopuksi oma item-taulu.
+create table item (
+      id int auto_increment primary key,
+      nimi         varchar(100) null,
+	  maa          varchar(40)  null,
+      vihje1       varchar(255) null,
+      vihje2       varchar(255) null,
+      vihje3       varchar(255) null
+  )
+	charset=latin1;
+
+#ryhmän 3 Ohjlemisto 1 ohjelmistopeli-tietokanta on nyt luotu ja valmis.
