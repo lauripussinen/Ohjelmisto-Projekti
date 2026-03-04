178893a582764bf53b8d0f8fc214572e63effb11
pieta <pietarus@metropolia.fi>
2026-03-04 09:51:53 +0200

    l

--- pietarin kauniit koodit/päästöt.py
+++ pietarin kauniit koodit/päästöt.py
@@ -1,11 +1,3 @@
-#funktio hakee tietokannasta lentokentän nimen ja koordinaatit
-def lentokentat(icao):
-    sql = "SELECT ident, name, latitude_deg, longitude_deg FROM airport WHERE ident = %s"
-    cursor = yhteys.cursor(dictionary=True)
-    cursor.execute(sql, (icao,))
-    result = cursor.fetchone()
-    return result
-
 # funktio laskee kahden lentokentän etäisyyden.
 def etaisyys(icao1, icao2):
     kentta1 = lentokentat(icao1)
