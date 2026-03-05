206b18edefe5adb5198a2360a948124b2c5e3f5e
lauripussinen <lauri.pussinen@gmail.com>
2026-03-05 17:55:43 +0200

    Päivityksiä Laurin juttuihin

--- Laurin jutut/Pelin tallenusfunktio.py
+++
@@ -1,4 +0,0 @@
-def tallenna_peli(tähän tarvii oikeet parametrit):
-    sql = f'UPDATE game SET location = %s, player_range = %s, money = %s WHERE id = %s'
-    cursor = conn.cursor(dictionary=True)
-    cursor.execute(sql, (tähän samat parametrit kuin funktion kutsussa))
